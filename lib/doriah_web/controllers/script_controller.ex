860baa06b081839b8557c8142f3e575808e8ce84
spookyorange <korkmazcoltu@mail.ru>
2024-04-11 13:42:57 +0300

    feat: download script as sh file

--- lib/doriah_web/controllers/script_controller.ex
+++ lib/doriah_web/controllers/script_controller.ex
@@ -5,4 +5,8 @@ defmodule DoriahWeb.ScriptController do
   def get_script(conn, %{"id" => id}) do
     text(conn, Scripting.get_script_as_sh_file(id))
   end
+
+  def get_script_download(conn, %{"id" => id}) do
+    send_download(conn, {:binary, Scripting.get_script_as_sh_file(id)}, filename: "script.sh")
+  end
 end

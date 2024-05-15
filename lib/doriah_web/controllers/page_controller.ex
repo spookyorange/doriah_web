bdbc23a7b6ad43017b8e6965e7bc1561503b176f
spookyorange <82583304+spookyorange@users.noreply.github.com>
2024-05-15 14:57:42 +0300

    home now redirects to scripts

--- lib/doriah_web/controllers/page_controller.ex
+++ lib/doriah_web/controllers/page_controller.ex
@@ -4,6 +4,6 @@ defmodule DoriahWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: ~p"/scripts")
   end
 end

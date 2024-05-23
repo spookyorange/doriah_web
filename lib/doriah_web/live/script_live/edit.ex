9402868e371c5e4748174604f4ddb9c4c2dcceb0
spookyorange <82583304+spookyorange@users.noreply.github.com>
2024-05-23 15:57:50 +0300

    remove unnecessary old code

--- lib/doriah_web/live/script_live/edit.ex
+++ lib/doriah_web/live/script_live/edit.ex
@@ -2,7 +2,6 @@ defmodule DoriahWeb.ScriptLive.Edit do
   use DoriahWeb, :live_view
   use DoriahWeb.BaseUtil.Controlful
 
-  import DoriahWeb.ScriptLive.Variable.Index
   alias Doriah.Scripting
 
   @impl true
@@ -32,83 +31,15 @@ defmodule DoriahWeb.ScriptLive.Edit do
     socket
   end
 
-  defp apply_action(socket, :import, _script) do
-    socket
-    |> assign(:import_text, "")
-    |> assign(:import_text_height, 1)
-  end
-
-  defp apply_action(socket, :variables, script) do
-    socket
-    |> stream(:script_variables, script.script_variables)
-  end
-
-  defp apply_action(socket, :basic_info, _script) do
-    socket
-  end
-
   defp get_row_count_of_textarea(area_value) do
     String.split(area_value, "\n") |> length()
   end
 
+  @impl true
   def handle_info({DoriahWeb.ScriptLive.FormComponent, {:saved, script}}, socket) do
     {:noreply,
      socket
      |> assign(:script, script)}
-  end
-
-  @impl true
-  def handle_info({DoriahWeb.ScriptLive.Variable.Card, {:saved, script_variable}}, socket) do
-    {:noreply,
-     stream_insert(socket, :script_variables, script_variable)
-     |> assign(:script_variables, socket.assigns.streams.script_variables)}
-  end
-
-  @impl true
-  def handle_info({DoriahWeb.ScriptLive.Variable.CreationForm, {:saved, script_variable}}, socket) do
-    {:noreply,
-     socket
-     |> stream_insert(:script_variables, script_variable)
-     |> push_event("reset-all-inputs-of-a-form", %{id: "script-variable-form"})}
-  end
-
-  @impl true
-  def handle_event(
-        "delete_variable",
-        %{
-          "deleted-variable-dom-id" => deleted_variable_dom_id,
-          "deleted-variable-self-id" => deleted_variable_self_id
-        },
-        socket
-      ) do
-    script_variable = Scripting.get_script_variable!(deleted_variable_self_id)
-    {:ok, _} = Scripting.delete_script_variable(script_variable)
-
-    {:noreply, socket |> stream_delete_by_dom_id(:script_variables, deleted_variable_dom_id)}
-  end
-
-  def handle_event(
-        "import_input_change",
-        %{"script_import" => %{"import_textarea" => input_value}},
-        socket
-      ) do
-    {:noreply,
-     socket
-     |> assign(:import_text, input_value)
-     |> assign(:import_text_height, get_row_count_of_textarea(input_value))}
-  end
-
-  def handle_event("submit_import_script", _params, socket) do
-    {:ok, updated_script} =
-      Scripting.import_sh_script(socket.assigns.script.id, socket.assigns.import_text)
-
-    {:noreply,
-     socket
-     |> assign(:whole_script, updated_script.whole_script)
-     |> assign(:import_text, "")
-     |> clear_flash()
-     |> put_flash(:info, "Script imported successfully!")
-     |> push_redirect(to: ~p"/scripts/#{socket.assigns.script.id}/edit_mode")}
   end
 
   def handle_event(
@@ -145,28 +76,6 @@ defmodule DoriahWeb.ScriptLive.Edit do
       {:noreply,
        socket
        |> push_redirect(to: ~p"/scripts/#{socket.assigns.script}/edit_mode/basic_info")
-       |> escape_controlful_and_keyboarder()}
-    else
-      {:noreply, socket}
-    end
-  end
-
-  def handle_event("keydown", %{"key" => "v"}, socket) do
-    if socket.assigns.keyboarder && socket.assigns.live_action == :edit_mode do
-      {:noreply,
-       socket
-       |> push_redirect(to: ~p"/scripts/#{socket.assigns.script}/variable_loadout")
-       |> escape_controlful_and_keyboarder()}
-    else
-      {:noreply, socket}
-    end
-  end
-
-  def handle_event("keydown", %{"key" => "i"}, socket) do
-    if socket.assigns.keyboarder && socket.assigns.live_action == :edit_mode do
-      {:noreply,
-       socket
-       |> push_redirect(to: ~p"/scripts/#{socket.assigns.script}/edit_mode/import")
        |> escape_controlful_and_keyboarder()}
     else
       {:noreply, socket}
@@ -221,6 +130,4 @@ defmodule DoriahWeb.ScriptLive.Edit do
 
   defp page_title(:edit_mode), do: "Script - Edit"
   defp page_title(:basic_info), do: "Script - Edit Basic Information"
-  defp page_title(:variables), do: "Script - Manage Variables"
-  defp page_title(:import), do: "Script - Import Script"
 end
